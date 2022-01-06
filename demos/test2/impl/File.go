67594c4f3e9f238fd7133a668e3bdc9246818453
hailang <hailang@webuy.ai>
2022-01-06 08:21:03 +0800

    完善spi实现

---
+++ demos/test2/impl/File.go
@@ -0,0 +1,5 @@
+package impl
+
+type Test struct {
+	Test func()
+}
--- dubbo-go-spi/api/extension_loader.go
+++ dubbo-go-spi/api/extension_loader.go
@@ -3,23 +3,28 @@ package api
 import (
 	"bytes"
 	"errors"
-	"fmt"
+	"github.com/modern-go/reflect2"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"plugin"
+	"reflect"
 	"runtime"
 	"strings"
 )
 
-// interfaces interfaceName : spiName : impl or .so or .dll
-var interfaces = make(map[string]map[string]*plugin.Plugin)
+// SPI配置文件根目录，类似java的 META-INF/services
+var configRoot = "./config"
+
+// allExtension interfaceType : spiName : impl or .so or .dll
+var allExtension = make(map[reflect2.Type]map[string]*plugin.Plugin)
+var cache = make(map[*plugin.Plugin]plugin.Symbol)
 
 // 初始化
 func init() {
-	err := filepath.Walk("./config",
+	err := filepath.Walk(configRoot,
 		func(path string, f os.FileInfo, err error) error {
 			if f == nil {
 				return err
@@ -31,9 +36,20 @@ func init() {
 			if err != nil {
 				return err
 			}
-			name := file.Name()
-			//todo 校验接口的合理性
-			fmt.Println(name)
+			split := strings.Split(file.Name(), "@")
+			if len(split) != 2 {
+				//非法配置文件
+				return nil
+			}
+			pkg := strings.ReplaceAll(split[0], "#", "/")
+			structName := split[1]
+			//todo 这里只能映射已发布的struct
+			//	   如果有interface必须用struct代替
+			theType := reflect2.TypeByPackageName(pkg, structName)
+			if theType == nil {
+				//struct不存在
+				return nil
+			}
 			data, err := ioutil.ReadFile(path)
 			if err != nil {
 				return err
@@ -43,8 +59,12 @@ func init() {
 				split := strings.Split(line, "=")
 				spiName := split[0]
 				lib := split[1]
-				if strings.Contains(lib, ".") {
+				if strings.LastIndex(lib, ".go") == len(lib)-3 {
+					//需要编译
 					lib = BuildDynamicLibrary(lib, lib)
+				} else {
+					//不需要编译
+					lib = GetDynamicLibraryName(lib)
 				}
 				plug, err := plugin.Open(lib)
 				if err != nil {
@@ -52,7 +72,7 @@ func init() {
 				}
 				extensions[spiName] = plug
 			}
-			interfaces[name] = extensions
+			allExtension[theType] = extensions
 			return nil
 		})
 	if err != nil {
@@ -60,35 +80,62 @@ func init() {
 	}
 }
 
-// GetExtension 获取扩展
-func GetExtension(interfaceName string, spiName string) (*plugin.Plugin, error) {
-	i := interfaces[interfaceName]
+// GetExtensionByType 获取扩展
+func GetExtensionByType(interfaceType reflect.Type, spiName string) (*plugin.Plugin, error) {
+	theType := reflect2.Type2(interfaceType)
+	i := allExtension[theType]
 	if i != nil {
 		extension := i[spiName]
 		if extension != nil {
 			return extension, nil
 		}
 	}
-	return nil, errors.New(interfaceName + " extension:" + spiName + " not find")
+	return nil, errors.New(interfaceType.String() + " extension:" + spiName + " not find")
+}
+
+// GetExtensionByName 获取扩展
+func GetExtensionByName(pkg string, structName string, spiName string) (*plugin.Plugin, error) {
+	theType := reflect2.TypeByPackageName(pkg, structName)
+	i := allExtension[theType]
+	if i != nil {
+		extension := i[spiName]
+		if extension != nil {
+			return extension, nil
+		}
+	}
+	return nil, errors.New(pkg + " " + structName + " extension:" + spiName + " not find")
+}
+
+func Lookup(plugin *plugin.Plugin, methodName string) (plugin.Symbol, error) {
+	if plugin == nil || len(methodName) <= 0 {
+		return nil, errors.New("非法参数")
+	}
+	if cache[plugin] == nil {
+		symbol, err := plugin.Lookup(methodName)
+		if err != nil {
+			return nil, err
+		}
+		cache[plugin] = symbol
+	}
+	return cache[plugin], nil
 }
 
 // LookupMethod 根据方法名称查找方法
-func LookupMethod(interfaceName string, spiName string, methodName string) (plugin.Symbol, error) {
-	extension, _ := GetExtension(interfaceName, spiName)
-	if extension != nil {
-		return extension.Lookup(methodName)
+func LookupMethod(interfaceType reflect.Type, spiName string, methodName string) (plugin.Symbol, error) {
+	extension, _ := GetExtensionByType(interfaceType, spiName)
+	method, err := Lookup(extension, methodName)
+	if err != nil {
+		return nil, err
 	}
-	return nil, errors.New("method:" + methodName + " can not find in:" + interfaceName + " " + spiName)
+	if method == nil {
+		return nil, errors.New("method:" + methodName + " can not find in:" + interfaceType.String() + " " + spiName)
+	}
+	return method, nil
 }
 
 // BuildDynamicLibrary 构建动态链接库
 func BuildDynamicLibrary(targetPath string, sourcePath string) string {
-	split := strings.Split(targetPath, ".")
-	if runtime.GOOS == "windows" {
-		targetPath = split[0] + ".dll"
-	} else {
-		targetPath = split[0] + ".so"
-	}
+	targetPath = GetDynamicLibraryName(targetPath)
 	cmd := exec.Command(
 		"go", "build",
 		"-buildmode", "plugin",
@@ -100,6 +147,16 @@ func BuildDynamicLibrary(targetPath string, sourcePath string) string {
 	cmd.Stderr = &out
 	if err := cmd.Run(); err != nil {
 		log.Fatalf("Error building plugin: %s\nOutput:\n%s", err, out.String())
+	}
+	return targetPath
+}
+
+func GetDynamicLibraryName(targetPath string) string {
+	split := strings.Split(targetPath, ".")
+	if runtime.GOOS == "windows" {
+		targetPath = split[0] + ".dll"
+	} else {
+		targetPath = split[0] + ".so"
 	}
 	return targetPath
 }
